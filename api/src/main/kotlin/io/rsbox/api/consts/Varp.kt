cb61f9a24b8de6c8f5b9b81277d4244e69bf27cf
pitchblac23 <tylermincks@hotmail.com>
2019-08-13 01:10:24 -0500

    updated varps

--- api/src/main/kotlin/io/rsbox/api/consts/Varp.kt
+++ api/src/main/kotlin/io/rsbox/api/consts/Varp.kt
@@ -3,14 +3,37 @@ package io.rsbox.api.consts
 object Varp {
     const val ACCEPT_AID = 427
     const val SCREEN_BRIGHTNESS = 166
+    const val DISABLE_MOUSEWHEEL_ZOOM_VARBIT = 6357
     const val MUSIC_VOLUME = 168
     const val SFX_VOLUME = 169
     const val ASX_VOLUME = 872
     const val CHAT_EFFECTS = 171
     const val SPLIT_PRIVATE = 287
+    const val HIDE_PM_WHEN_CHAT_HIDDEN_VARBIT = 4089
     const val PROFANITY_VARP = 1074
+    const val IDLE_NOTIFICATION_VARBIT = 1627
     const val MOUSE_BUTTONS = 170
+    const val MOUSE_CAMERA_VARBIT = 4134
+    const val PET_OPTIONS_VARBIT = 5599
+    const val SHIFT_CLICK_DROP_VARBIT = 5542
     const val PLAYER_ATTACK_PRIORITY = 1107
     const val NPC_ATTACK_PRIORITY = 1306
+    const val ATTACK_STYLE_VARP = 43
+    const val DISABLE_AUTO_RETALIATE_VARP = 172
+    const val CHATBOX_SCROLLBAR_VARBIT = 6374
+    const val DISABLE_SIDEPANELS_OPAQUE_VARBIT = 4609
+    const val DISABLE_XP_TILL_LEVEL_VARBIT = 4181
+    const val DISABLE_PRAYER_TOOLTIP_VARBIT = 5711
+    const val DISABLE_SPECIAL_ATTACK_TOOLTIP_VARBIT = 5712
+    const val HIDE_DATA_ORBS_VARBIT = 4084
+    const val CHATBOX_TRANSPARENT_VARBIT = 4608
+    const val CHATBOX_SOLID_VARBIT = 2570
+    const val SIDESTONES_ARRAGEMENT_VARBIT = 4607
+    const val CLOSE_TABS_WITH_HOTKEY_VARBIT = 4611
+    const val SPECIAL_ATTACK_VARP = 301
+    const val CANOE_STATION_CHANGES = 674
+    private const val SPECIAL_ATTACK_ENERGY_VARP = 300
+    private const val RUN_ENABLED_VARP = 173
+    private const val HP_ORB_VARP = 102
 
 }
